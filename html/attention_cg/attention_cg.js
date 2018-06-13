17e573d9b8458c1666379703eea93f9bac5cabe8
zhiry <zhiry@yonyou.com>
2018-06-13 16:48:30 +0800

    采购关注

--- html/attention_cg/attention_cg.js
+++ html/attention_cg/attention_cg.js
@@ -70,24 +70,16 @@ summerready = function(){
                 "addBackListener":"true"
             });
 		},
-		addToCart:function(data){
-			var mCode = data.mCode;
-	        var fCode = data.fCode;
-	        var fName = data.fName;
-	        if(data.stock=='-'){
-	        	summer.toast({
-                     "msg" : "库存为零" 
-                })
-	        	return;
-	        }
+		addToCg:function(item){
+	        var favm = item;
 	        var p_conditions = {};
-	        p_conditions['amount'] = 1;
-	        p_conditions['fcode'] = fCode;
-	        p_conditions['mcode'] = mCode;
-	        p_conditions['cartstype'] = '1';
+	        p_conditions['suStoreCode'] = favm['suStoreCode'];
+	        p_conditions['ieopEnterpriseCode'] = favm['ieopEnterpriseCode'];
+	        p_conditions['materialCode'] = favm['materialCode'];
+	        p_conditions['buyStoreSwitch'] = '0';
+	        p_conditions['addAmount'] = '1';
 	        var enc_conditions = p_page_params_con_dataj_enc(p_conditions,{},{});
-	        p_async_post(ip+'/ieop_base_mobile/mfrontmallusercarts/save', enc_conditions,'addToCart');
-	       
+	        var data = p_async_post(ip+'/ieop_base_mobile/mfrontsumallusercarts/save', enc_conditions , 'addUserCart');
 		},
 		chooseOrg:function(item){
 				var p_conditions = {
@@ -138,6 +130,19 @@ function initPage (keyW){
     var paramData = p_page_params_con_dataj_enc(p_conditions,page_params,sortItem);
     p_async_post(ip+'/ieop_base_mobile/mfrontsumallmaterialfavorites/querypage', paramData,'querypage');
 }
+function addUserCart(data){
+	if(data.status == 1){
+		summer.setStorage("cartType", 1);
+        summer.openWin({
+            "id" : "cart_cg",
+            "url" : "html/cart_cg/cart_cg.html"
+        });
+    }else{
+        summer.toast({
+            "msg":data.msg
+        })
+    }
+}
 function queryuserfactories(res){
 	var factories = res;
     if(factories.status==1){
@@ -151,10 +156,59 @@ function queryuserfactories(res){
 }
 function querypage(res){
 	if(res.status==1){
+        var orderData = res.retData.ents;
+        var suMCodes = "";
+        var suStoreCodes = "";
+        var ieopEnterpriseCodes = "";
+		for(var i = 0; i < orderData.length; i++){
+            var ent = orderData[i];
+            ent.showAdd = true;
+            suMCodes += ent.materialCode + "#";
+            suStoreCodes += ent.suStoreCode + "#";
+            ieopEnterpriseCodes += ent.ieopEnterpriseCode + "#";
+        }
+        suMCodes = suMCodes.substring(0,suMCodes.length-1);
+        suStoreCodes = suStoreCodes.substring(0,suStoreCodes.length-1);
+        ieopEnterpriseCodes = ieopEnterpriseCodes.substring(0,ieopEnterpriseCodes.length-1);
+        var info = {};
+        info['suMCodes'] = suMCodes;
+        info['suStoreCodes'] = suStoreCodes;
+        info['ieopEnterpriseCodes'] = ieopEnterpriseCodes;
+        var bb = p_params_con_dataj_enc(info);
         console.log(res.retData.ents)
+		viewModel.attentionList(orderData);
+        var data = p_async_post(ip+'/ieop_base_mobile/mfrontsustorematerial/querybymescodes', bb ,'querybymescodes');
     }else {
         summer.toast({
             "msg":res.msg
+        })
+    }
+}
+function querybymescodes(data){
+     if(data.status==1){
+        var refents = data.retData.ents;
+        var refObj = {};
+        for(var j=0;j<refents.length;j++){
+            var ent = refents[j];
+            var key = ent.suMCode+"#"+ent.suStoreCode+"#"+ent.ieopEnterpriseCode;
+            refObj[key] = ent;
+        }
+        var attentionList = viewModel.attentionList();
+        for(var i=0;i<attentionList.length;i++){
+            var key = attentionList[i].materialCode+"#"+attentionList[i].suStoreCode+"#"+attentionList[i].ieopEnterpriseCode;
+            if(refm!=undefined){
+                suMRefCode = refm.suMRefCode;
+            }
+            if(refm==undefined){
+                attentionList[i].showAdd = false;
+            }else{
+                attentionList[i].showAdd = true;
+            }
+        }
+        viewModel.attentionList(attentionList);
+    }else{
+        summer.toast({
+            "msg":data.msg
         })
     }
 }
@@ -170,13 +224,4 @@ function setuserfactory(result){
     }
     $('.org-list').slideUp();
     $('.drop').hide();
-}
-function addToCart(data){
-	 if(data.status==1){
-	    openWin1('cart');
-	 }else{
-	    summer.toast({
-            "msg" : "加入仓库失败" 
-        })
-	 }
 }
