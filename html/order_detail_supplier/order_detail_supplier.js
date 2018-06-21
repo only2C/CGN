ef4a2575d23df7ba96453db5d715c3ca673eee04
only <776268047@qq.com>
2018-06-21 17:51:53 +0800

    bug

--- html/order_detail_supplier/order_detail_supplier.js
+++ html/order_detail_supplier/order_detail_supplier.js
@@ -96,7 +96,7 @@ summerready = function () {
         openWin: function (options, data) {
             summer.openWin({
                 "id": "detail",
-                "url": "html/detail/detail.html",
+                "url": "html/detail_supplier/detail_supplier.html",
                 "pageParam": {
                     options: options
                 }
@@ -294,11 +294,13 @@ function queryaggsingle(res) {
         viewModel.nullDataTips(true);
         return ;
     }
+    var childOrder = res.retData.aggEnt.children.su_mall_order_infos;
     if (summer.pageParam.type == 'send_back') {
-        var childOrder = res.retData.aggEnt.children.mall_transfer_order_ret;
-    } else {
-        var childOrder = res.retData.aggEnt.children.su_mall_order_infos;
+        childOrder = res.retData.aggEnt.children.mall_transfer_order_ret;
     }
+    childOrder.forEach(function (val) {
+        val.materialImgUrl = val.materialImgUrl ? summer.getStorage("imgBaseUrl")+val.materialImgUrl:'';
+    })
 
     var mainEnt = res.retData.aggEnt.mainEnt;
     if (mainEnt.returnUrgent == '0') {
